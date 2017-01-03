44186aedc1564861497eb995980de590bad8e488
Casey Cross <hellocaseycross@gmail.com>
2017-01-02 20:56:15 -0700

    Update mock users.

--- test/helpers/mockUsers.js
+++ test/helpers/mockUsers.js
@@ -1,30 +1,30 @@
 const mockUsers = [
   {
       user: {
-          displayName: "Casey Cross",
-          email: "hellocaseycross@gmail.com",
-          uid: "oSMIlUTN5OZOVY9pYfEi1PXCv9q1"
+          displayName: 'Casey Cross',
+          email: 'hellocaseycross@gmail.com',
+          uid: 'oSMIlUTN5OZOVY9pYfEi1PXCv9q1'
       }
   },
   {
       user: {
-          displayName: "Jeff Duke",
-          email: "jeff.duke@gmail.com",
-          uid: "8opUzbSXdxOJcKgukMF4oIMPTu12"
+          displayName: 'Jeff Duke',
+          email: 'jeff.duke@gmail.com',
+          uid: '8opUzbSXdxOJcKgukMF4oIMPTu12'
       }
   },
   {
       user: {
-          displayName: "Andy Young",
-          email: "atyoungdesign@gmail.com",
-          uid: "aL0MeDz7oXRqtSBPRYguthVf5aw2"
+          displayName: 'Andy Young',
+          email: 'atyoungdesign@gmail.com',
+          uid: 'aL0MeDz7oXRqtSBPRYguthVf5aw2'
       }
   },
   {
       user: {
-          displayName: "Brett Hevia",
-          email: "brett.hevia@gmail.com",
-          uid: "vOaZV0E03SRRyGnpE8XMm6koZZA3"
+          displayName: 'Brett Hevia',
+          email: 'brett.hevia@gmail.com',
+          uid: 'vOaZV0E03SRRyGnpE8XMm6koZZA3'
       }
   },
   {
